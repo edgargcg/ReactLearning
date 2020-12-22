d41b16c5fe301dc7a79afb03dee5576212eaa9c2
Edgar Campos <edgargcg@gmail.com>
2020-12-22 13:19:28 -0600

    04-gif-expert-app adding animation on gif grid item

--- 04-gif-expert-app/src/Components/Gifs/GifGridItem.js
+++ 04-gif-expert-app/src/Components/Gifs/GifGridItem.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export const GifGridItem = ({id, title, url}) => {
     return (
-        <div className="images__item">
+        <div className="images__item animate__animated animate__bounce">
             <h5 className="image__title">
                 {title}
             </h5>
